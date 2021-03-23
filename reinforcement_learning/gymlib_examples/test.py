6e20b5d293af273f462aa8453762f7d2ce7bf4e4
rishimalhan <rmalhan0112@gmail.com>
2021-03-22 22:04:27 -0700

    Trying out stock trading using RL

---
+++ reinforcement_learning/gymlib_examples/test.py
@@ -0,0 +1,22 @@
+# import gym
+
+# env = gym.make('CartPole-v0')
+# obs = env.reset()
+# print(env.observation_space.low)
+# print(env.observation_space.high)
+
+# # obs, reward, done, info = env.step( env.action_space.sample() )
+# # print(reward)
+# # print(done)
+
+# env.close()	
+# # print( env.action_space )
+
+
+
+from gym import spaces
+space = spaces.Discrete(8) # Set with 8 elements {0, 1, 2, ..., 7}
+x = space.sample()
+print(x)
+assert space.contains(x)
+assert space.n == 8
